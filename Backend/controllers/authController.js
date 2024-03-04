92285aeb8046112d2a424f44733a7899c2253e9b
AnmolSonkar <anmolsonkar742@gmail.com>
2024-03-04 20:24:21 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -51,8 +51,7 @@ const signup_post = async (req, res, next) => {
 
         const user = await User.create({ email, password });
         const token = createToken(user._id);
-        res.cookie('jwt', token, { withCredentials: true, httpOnly: true, maxAge: maxAge * 1000, sameSite: 'none',secure: true,  partitioned: true
-    });
+        res.cookie('jwt', token, { withCredentials: true, httpOnly: true, maxAge: maxAge * 1000, sameSite: 'none' });
         res.status(201).json({ user: user._id, created: true });
     } catch (error) {
         const errors = handleErrors(error);
@@ -65,8 +64,7 @@ const login_post = async (req, res) => {
     try {
         const user = await User.login(email, password);
         const token = createToken(user._id);
-        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000, sameSite: 'none',secure: true,  partitioned: true
-    });
+        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000, sameSite: 'none' });
         res.status(200).json({ user: user._id, status: true });
     } catch (error) {
         const errors = handleErrors(error);
